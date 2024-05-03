c5cf5ffeb315a5f5732e9c38a8737ad1ad5e7e4f
amithkashyap <amithkashyaprv@gmail.com>
2024-05-04 01:06:31 +0530

    added UI for Sidenav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+import { useSidenavState } from "../context/SidenavContext";
+
+const StyledSidenav = styled.nav`
+  height: 100vh;
+  width: 25rem;
+  background-image: linear-gradient(to bottom, black, grey);
+  left: ${(props) => (props.display ? "5rem" : "-20rem")};
+  transition: 0.8s;
+  transform: translateX(${(props) => (props.display ? "0rem" : "-20rem")});
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SidenavHeading = styled.h1`
+  text-align: center;
+  padding: 2rem;
+  color: #deaa86;
+  font-weight: normal;
+  font-size: xx-large;
+`;
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  const value = useSidenavState();
+
+  return (
+    <StyledSidenav display={value.isSidenavOpen}>
+      <SidenavHeading>Quick Link</SidenavHeading>
+      <SidenavList>
+        <SidenavListItem>Home</SidenavListItem>
+        <SidenavListItem>About Me</SidenavListItem>
+        <SidenavListItem>Work Experience</SidenavListItem>
+        <SidenavListItem>Technologies</SidenavListItem>
+        <SidenavListItem>Projects</SidenavListItem>
+        <SidenavListItem>Extra Curricular</SidenavListItem>
+      </SidenavList>
+    </StyledSidenav>
+  );
 };
 
 export default Sidenav;
