d5dd47afb25fb813dde5d3f0f206afd7bc4e511d
amithkashyap <amithkashyaprv@gmail.com>
2024-05-05 01:17:12 +0530

    added work experience data array

---
+++ src/data/workExperience.js
@@ -0,0 +1,18 @@
+const workExperience = [
+  {
+    companyName: "PharmEasy",
+    companyLogo: "",
+    designation: "Senior Software Engineer",
+    startDate: "Oct 2020",
+    endDate: "July 2023",
+    highlights: [],
+  },
+  {
+    companyName: "PayPal",
+    companyLogo: "",
+    designation: "Senior Software Engineer",
+    startDate: "Oct 2020",
+    endDate: "July 2023",
+    highlights: [],
+  },
+];
