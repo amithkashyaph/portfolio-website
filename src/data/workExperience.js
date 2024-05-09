245bca0befe3a3841a56081bbdacd26e2d9c9e4a
amithkashyap <amithkashyaprv@gmail.com>
2024-05-10 01:54:39 +0530

    added work experience highlights

--- src/data/workExperience.js
+++ src/data/workExperience.js
@@ -1,7 +1,7 @@
 export const workExperience = [
   {
     companyName: "Vimeo",
-    companyLogo: "",
+    companyLogo: "/vimeo.png",
     designation: "Senior Software Engineer",
     startDate: "Aug 2023",
     endDate: "Present",
@@ -9,16 +9,22 @@ export const workExperience = [
   },
   {
     companyName: "PharmEasy",
-    companyLogo: "",
+    companyLogo: "/PharmEasy.png",
     designation: "Senior Software Engineer",
     startDate: "Oct 2020",
     endDate: "July 2023",
-    highlights: [],
+    highlights: [
+      "Developed, owned and managed critical services including promotion engines, loyalty programs, user ratings and feedback, user segmentation, offer discovery and SEO. Maintained an average latency of approx 10ms serving at a combined throughput of 200Mn+ requests per week.",
+      "Designed and developed business critical features like combined checkout of loyalty programs with medicines, auto application of coupons on cart, graded and variable coupons/promotions, SEO optimized content management for pharma and diagnostics products. This has resulted into approx 12% higher loyalty enrollments ,13% higher order placements and 1.5X organic traffic increase",
+      "Optimized code, resource utilization and Kubernetes configurations to improve performance, scalability and infra cost savings. Achieved 45% reduced infra cost and 3X lower latency. Rearchitected user segmentation service to reduce overall segment update timeline by 60%.",
+      "Designed, developed and owned multiple UI elements to enable business and ops to take care of their regular activities without any tech intervention. Coached and onboarded peers, performed regular code and design reviews to ensure code quality and design paradigms. Worked with PMs and other stakeholders to establish efficient SOPs for debugging and addressing customer grievances",
+      "Implemented multiple alerts and monitoring dashboards including observability, API performance metrics and resource metrics etc. Performed RCAs for production outages with actionables",
+    ],
   },
   {
     companyName: "PayPal",
-    companyLogo: "",
-    designation: "Senior Software Engineer",
+    companyLogo: "/paypal.png",
+    designation: "Software Engineer I",
     startDate: "Oct 2020",
     endDate: "July 2023",
     highlights: [],
