517aaf8709ce12f07acc2ded26ba1c86d0436855
amithkashyap <amithkashyaprv@gmail.com>
2024-05-09 22:16:55 +0530

    updated work experience data

--- src/data/workExperience.js
+++ src/data/workExperience.js
@@ -1,5 +1,13 @@
 const workExperience = [
   {
+    companyName: "Vimeo",
+    companyLogo: "",
+    designation: "Senior Software Engineer",
+    startDate: "Aug 2023",
+    endDate: "Present",
+    highlights: [],
+  },
+  {
     companyName: "PharmEasy",
     companyLogo: "",
     designation: "Senior Software Engineer",
