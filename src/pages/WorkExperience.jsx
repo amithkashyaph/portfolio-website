ca4ede1f1d4daa4987431aada621a64b3d1b6e65
amithkashyap <amithkashyaprv@gmail.com>
2024-05-09 23:32:34 +0530

    added simple styles to WorkExperience component

--- src/pages/WorkExperience.jsx
+++ src/pages/WorkExperience.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import { workExperience } from "../data/workExperience";
+import Accordion from "../components/Accordion";
 
 const WorkExperience = () => {
   const AccordionContainer = styled.div`
-    width: 70%;
+    padding: 1rem;
+    text-align: center;
+    width: 100%;
+    margin: 1rem 0 1rem 5rem;
   `;
-  return <AccordionContainer></AccordionContainer>;
+
+  const ExperinceHeader = styled.h1`
+    text-align: center;
+    margin: 1rem 0 1rem 5rem;
+  `;
+  return (
+    <>
+      <ExperinceHeader>Work Experience</ExperinceHeader>
+      <AccordionContainer>
+        {workExperience.map((experience) => (
+          <Accordion />
+        ))}
+      </AccordionContainer>
+    </>
+  );
 };
 
 export default WorkExperience;
