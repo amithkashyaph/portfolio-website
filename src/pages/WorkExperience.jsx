c2632bdf71d1a8f691d36934974071e6e5f4aebf
amithkashyap <amithkashyaprv@gmail.com>
2024-05-09 22:15:51 +0530

    added initial work experience component

---
+++ src/pages/WorkExperience.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const WorkExperience = () => {
+  const AccordionContainer = styled.div`
+    width: 70%;
+  `;
+  return <AccordionContainer></AccordionContainer>;
+};
+
+export default WorkExperience;
