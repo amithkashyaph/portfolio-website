5ba14ad2747fca199d3fd97fb63048e8c5c5f338
amithkashyap <amithkashyaprv@gmail.com>
2024-05-09 22:17:21 +0530

    added work experience component in the main app layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Sidebar from "./components/Sidebar";
 import Sidenav from "./components/Sidenav";
 import { SidenavContextProvider } from "./context/SidenavContext";
 import Home from "./pages/Home";
+import WorkExperience from "./pages/WorkExperience";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Sidebar />
         <Sidenav />
         <Home />
+        <WorkExperience />
       </SidenavContextProvider>
     </>
   );
