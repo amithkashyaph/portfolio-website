6b47f65a2a43c2fbbec280db215b4ed0c82c5126
amithkashyap <amithkashyaprv@gmail.com>
2024-05-10 01:10:06 +0530

    placed the styled elements outside the component

--- src/pages/WorkExperience.jsx
+++ src/pages/WorkExperience.jsx
@@ -3,24 +3,25 @@ import styled from "styled-components";
 import { workExperience } from "../data/workExperience";
 import Accordion from "../components/Accordion";
 
-const WorkExperience = () => {
-  const AccordionContainer = styled.div`
-    padding: 1rem;
-    text-align: center;
-    width: 100%;
-    margin: 1rem 0 1rem 5rem;
-  `;
+const AccordionContainer = styled.div`
+  padding: 1rem;
+  text-align: center;
+  width: 100%;
+  margin-left: 5rem;
+`;
 
-  const ExperinceHeader = styled.h1`
-    text-align: center;
-    margin: 1rem 0 1rem 5rem;
-  `;
+const ExperinceHeader = styled.h1`
+  text-align: center;
+  margin: 3rem 0 1rem 5rem;
+`;
+
+const WorkExperience = () => {
   return (
     <>
       <ExperinceHeader>Work Experience</ExperinceHeader>
       <AccordionContainer>
         {workExperience.map((experience) => (
-          <Accordion />
+          <Accordion experience={experience} />
         ))}
       </AccordionContainer>
     </>
