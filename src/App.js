5e3e1e530c4be5986ec753c17fd1478f25fefc54
amithkashyap <amithkashyaprv@gmail.com>
2024-05-29 21:42:16 +0530

    added Skills section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Sidebar from "./components/Sidebar";
 import Sidenav from "./components/Sidenav";
 import { SidenavContextProvider } from "./context/SidenavContext";
 import Home from "./pages/Home";
+import Skills from "./pages/Skills";
 import WorkExperience from "./pages/WorkExperience";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Sidebar />
         <Sidenav />
         <Home />
+        <Skills />
         <WorkExperience />
       </SidenavContextProvider>
     </>
